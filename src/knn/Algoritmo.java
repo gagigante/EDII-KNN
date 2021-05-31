78a99493e269b1084cb6c6d2cdd45c9f489f06d8
Gabriel Pereira <gabrielspereira2604@gmail.com>
2021-05-30 21:43:30 -0300

    Partes 2, 3 e 4

--- src/knn/Algoritmo.java
+++ src/knn/Algoritmo.java
@@ -5,6 +5,8 @@
  */
 package knn;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 
 /**
@@ -12,5 +14,46 @@ import java.util.LinkedList;
  * @author gabri
  */
 public class Algoritmo {
-    LinkedList<String[]> dataset = new LinkedList<>();
+    public LinkedList<String[]> dataset = new LinkedList<>();
+    
+    public float euclidianDistance(String currentReg, String target){
+        String[] currentVetor = currentReg.split(",");
+        String[] targ = target.split(",");
+        float somatorio = 0;
+        
+        for(int i = 0; i < currentVetor.length - 1; i++) {
+            float diferenca = Float.parseFloat(currentVetor[i]) - Float.parseFloat(targ[i]);
+            somatorio += Math.pow(diferenca, 2); 
+        }
+        
+        return (float)Math.sqrt(somatorio);
+    }
+    
+    public String classify(LinkedList<String[]> dataset, int k, String target){
+        if(dataset == null){
+            return null;
+        }
+        
+        if(k == 0){
+            k = 1;
+        }
+        
+        ArrayList<distanciaDoTarget> lista = new ArrayList();
+
+        for(String[] vetor : dataset){
+            String currentRegValor = "";
+
+            for(String valor : vetor){
+                currentRegValor += valor + ",";
+            }
+            
+            distanciaDoTarget d = new distanciaDoTarget();
+            
+            d.distancia = euclidianDistance(currentRegValor.substring(0, currentRegValor.length() - 1), target);
+            d.classe = vetor[vetor.length - 1];
+            lista.add(d);
+        }
+        
+        return "";
+    }
 }
--- src/knn/KNN.java
+++ src/knn/KNN.java
@@ -23,34 +23,66 @@ public class KNN {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-//      dataset.txt 
-//      Height,Weight,Age,Class
-//      1.70,65,20,Programmer
-//      1.90,85,33,Builder
-//      1.78,76,31,Builder
-//      1.73,74,24,Programmer
-//      1.81,75,35,Builder
-//      1.73,70,75,Scientist
-//      1.80,71,63,Scientist
-//      1.75,69,25,Programmer
+        LinkedList<String> datasetSemClasse = new LinkedList<>();
+
+        //      dataset.txt 
+        //      Height,Weight,Age,Class
+        //      1.70,65,20,Programmer
+        //      1.90,85,33,Builder
+        //      1.78,76,31,Builder
+        //      1.73,74,24,Programmer
+        //      1.81,75,35,Builder
+        //      1.73,70,75,Scientist
+        //      1.80,71,63,Scientist
+        //      1.75,69,25,Programmer
 
         try {
-            FileReader arquivo = new FileReader("d:\\dataset.txt");
+            FileReader arquivo = new FileReader("C:\\dataset.txt");
             BufferedReader lerArq = new BufferedReader(arquivo);
 
             String linha = lerArq.readLine(); // lê a primeira linha
             // a variável "linha" recebe o valor "null" quando o processo
             // de repetição atingir o final do arquivo texto
+            
+            String atributos[] = linha.split(","); //primeira linha (são os atributos)
+            String valores[];
+            
+            Boolean isAtributo = true;
+            
+            Algoritmo a = new Algoritmo();
+            
             while (linha != null) {
-                System.out.printf("%s\n", linha);
-
+                if(isAtributo){
+                    isAtributo = false;
+                }else{
+                    a.dataset.add(linha.split(","));
+                    datasetSemClasse.add(linha.substring(0, linha.lastIndexOf(",")));
+                }
+                
                 linha = lerArq.readLine(); // lê da segunda até a última linha
-            }
-
+            }       
+            
             arquivo.close();
+            
+            try{
+                for(String vetor : datasetSemClasse){
+                    for(String valor : vetor.split(",")){
+                        float num = Float.parseFloat(valor);
+                    }
+                }
+                
+                a.classify(a.dataset, 3, datasetSemClasse.get(4));
+            } catch (NumberFormatException e) {
+                System.out.println("Não pode ser transformado num tipo float");
+            }
+            
+            
         } catch (IOException e) {
             System.err.printf("Erro na abertura do arquivo: %s.\n",
             e.getMessage());
         }
+        
     }
+    
+    
 }
